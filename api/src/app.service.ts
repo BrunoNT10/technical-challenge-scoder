c675ac5bfef69f2b3134620d0c595e662f6c76be
BrunoNT10 <brunont1211@gmail.com>
2025-05-30 15:18:18 -0300

    improve: reconfigure the standard route

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getStandardRoute(): Record<string, string> {
+    return {"type": "success", "message": "The API was accessed succesfully!"};
   }
 }
